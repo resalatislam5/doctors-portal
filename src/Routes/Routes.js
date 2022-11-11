5d1f7e059b2b587ba3cfd5eae4618c40f7a2314e
Resalat <resalatislam5@gmail.com>
2022-11-12 03:24:05 +0600

    components and routers

---
+++ src/Routes/Routes.js
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main/Main";
+import About from "../pages/About/About";
+import Appointment from "../pages/Appointment/Appointment";
+import ContactUs from "../pages/ContactUs/ContactUs";
+import Home from "../pages/Home/Home/Home";
+import Reviews from "../pages/Reviews/Reviews/Reviews";
+import Login from "../pages/Sheard/Login/Login";
+//sherd
+import SignUp from "../pages/Sheard/SignUp/SignUp";
+
+export const routes = createBrowserRouter([
+    {
+        path:'/',
+        element: <Main />,
+        children:[
+            {
+                path:'/',
+                element: <Home />
+            },
+            {
+                path:'/about',
+                element: <About />
+            },
+            {
+                path:'/appointment',
+                element: <Appointment />
+            },
+            {
+                path:'/reviews',
+                element: <Reviews />
+            },
+            {
+                path:'/contact',
+                element: <ContactUs />
+            },
+            {
+                path:'/Login',
+                element: <Login />
+            },
+            {
+                path:'/signup',
+                element: <SignUp />
+            },
+        ]
+    }
+])
---
+++ src/layout/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../../pages/Sheard/Footer/Footer';
+import Header from '../../pages/Sheard/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet></Outlet>
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
