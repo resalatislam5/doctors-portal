ed4e6d807b9e2cc5aacb201bf73277b5da0a0bcb
Resalat <resalatislam5@gmail.com>
2022-11-13 03:11:11 +0600

    footer

--- src/pages/Sheard/Footer/Footer.js
+++ src/pages/Sheard/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import footerImg from '../../../assets/images/footer.png'
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div style={{
+            background: `url(${footerImg})`,
+            backgroundSize:'cover'
+            }}>
+            <footer className="footer p-10 justify-between">
+            <div>
+                <span className="footer-title">Services</span> 
+                <Link to='/'  className="link link-hover">Branding</Link>
+                <Link to='/'  className="link link-hover">Design</Link>
+                <Link to='/'  className="link link-hover">Marketing</Link>
+                <Link to='/'  className="link link-hover">Advertisement</Link>
+            </div> 
+            <div>
+                <span className="footer-title">Company</span> 
+                <Link to='/'  className="link link-hover">About us</Link>
+                <Link to='/'  className="link link-hover">Contact</Link>
+                <Link to='/'  className="link link-hover">Jobs</Link>
+                <Link to='/'  className="link link-hover">Press kit</Link>
+            </div> 
+            <div>
+                <span className="footer-title">Legal</span> 
+                <Link to='/'  className="link link-hover">Terms of use</Link>
+                <Link to='/'  className="link link-hover">Privacy policy</Link>
+                <Link to='/'  className="link link-hover">Cookie policy</Link>
+            </div>
+            </footer>
+            <p className='text-center py-8'>Copyright 2022 All Rights Reserved</p>
         </div>
     );
 };
